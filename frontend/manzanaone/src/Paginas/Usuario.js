ab04ac135e427bc04913b01623cc89836801866d
juliette097 <juliettedayannagonzalezg@gmail.com>
2025-10-22 16:03:12 -0500

    login y dependencias

--- frontend/manzanaone/src/Paginas/Usuario.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-function Usuario(){
-    return(
-        <h2>hola</h2>
-    )
-}
-export default Usuario;
--- frontend/manzanaone/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from 'react-dom/client'
-import App from './Componentes/App'
-
-
-const contenedor = document.getElementById('root');
-const root = createRoot(contenedor)
-root.render(<App/>)
